b7b45149a9ed363663731a873226920c4ad2edbc
sru07 <srujith07@srumail.com>
2025-03-11 18:04:50 +0000

    1st problem

---
+++ 1.py
@@ -0,0 +1,7 @@
+num_1 = int(input("Enter Number 1 value : "))
+num_2 = int(input("Enter Number 2 value : "))
+
+if ( num_1 * num_2 ) >= 1000:
+    print (num_1 + num_2 )
+else:
+    print( num_1*num_2 )
